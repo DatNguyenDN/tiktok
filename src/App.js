e21c26885a767e348706c3c0beb15f6e63099be7
Dat Nguyen <ncdat2610@gmail.com>
2023-11-02 00:07:56 +0700

    Cấu hình Router/Layouts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
     return (
-        <div className="App">
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        // xử lý biến Page để đưa vào tag vào element
+                        const Page = route.component;
 
-        </div>
-    
-    )
+                        let Layout = DefaultLayout;
 
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,3 @@
+export {default as DefaultLayout} from './DefaultLayout' //Xem lại ES6
+
+export {default as HeaderOnly} from './HeaderOnly' 
