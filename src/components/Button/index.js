ae9a5c976db3b0db0715fd23e9ff921ed7d2a07d
Dat Nguyen <ncdat2610@gmail.com>
2023-11-03 00:59:51 +0700

    UI Header #3 - Bài 98

---
+++ src/components/Button/index.js
@@ -0,0 +1,62 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+import { faL } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function Button({ 
+    to, 
+    href, 
+    primary = false, 
+    outline = false, 
+    text = false,
+    rounded = false,
+    disable = false,
+    small = false, 
+    large=false, 
+    children, 
+    className,
+    onClick,
+    leftIcon,
+    rightIcon,
+    ...passProps }) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps
+    };
+
+    if (disable)  {
+        delete props.onClick;
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+    const classes = cx('wrapper', {
+        [className]: className,// khi có className thì lấy giá trị của className làm key
+        primary,
+        outline,
+        text,
+        rounded,
+        disable,
+        small,
+        large,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '@/assets/images';
-
+import Button from '@/components/Button';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css'; // optional
 import { Wrapper as PopperWrapper } from '@/components/Popper';
@@ -70,7 +70,11 @@ function Header() {
                     </div>
                 </Tippy>
 
-                <div className={cx('action')}>ACTION</div>
+                <div className={cx('action')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                    
+                </div>
             </div>
         </header>
     );
