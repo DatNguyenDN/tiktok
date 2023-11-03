2cbe53febcf665441e3913e44873c1d3ee0ebbb0
Dat Nguyen <ncdat2610@gmail.com>
2023-11-03 13:41:32 +0700

    UI Header #5 - Bài 100

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -25,6 +25,19 @@ const MENU_ITEMS = [
     {
         icon:<FontAwesomeIcon icon={faGlobe} />,
         title:'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    code: 'en',
+                    title:'English'
+                },
+                {
+                    code: 'vi',
+                    title:'Tiếng Việt'
+                },
+            ]
+        }
     },
     {
         icon:<FontAwesomeIcon icon={faCircleQuestion} />,
@@ -45,6 +58,16 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+
+    // Handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+        }
+    }
 
     return (
         <header className={cx('wrapper')}>
@@ -96,7 +119,7 @@ function Header() {
                     <Button primary>Log in</Button>
                     
                     <Menu
-                        items={MENU_ITEMS}
+                        items={MENU_ITEMS} onChange={handleMenuChange}
                     >
                     <button className={cx('more-btn')}>
                         <FontAwesomeIcon icon={faEllipsisVertical}/>
